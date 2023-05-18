e7b974066bab87bea09a61c09659bf8049ace5e7
jakmike <asomughakenenna@gmail.com>
2023-05-18 19:30:52 +0000

    update on task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,13 @@
-file { '/etc/default/nginx':
-  ensure  => file,
-  content => "ULIMIT='-n 3000'\n",
-  sudo systemctl restart nginx,
+# This manuscript increases the number of traffic an Nginx server can handle
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Restart Nginx
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
